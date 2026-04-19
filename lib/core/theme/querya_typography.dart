1a545c285d5b2be6380552054148af0341b84596
ZhuchkaTriplesix <mrlololoshka94@gmail.com>
2026-04-19 12:42:50 +0300

    theme: shared monospace stack for SQL editors

---
+++ lib/core/theme/querya_typography.dart
@@ -0,0 +1,6 @@
+/// Monospace stack for SQL editors (bundled font can replace this later).
+abstract class QueryaTypography {
+  QueryaTypography._();
+
+  static const String mono = 'monospace';
+}
