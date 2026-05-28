1b8500154b3907b55159671d62059d02f29ddaa0
ZhuchkaTriplesix <mrlololoshka94@gmail.com>
2026-05-28 10:19:21 +0300

    feat(theme): add QueryaThemeScope for workbench and editor tokens
    Exposes context.workbench / context.editorTheme via InheritedWidget.
    Wraps app home with QueryaTheme.darkDefault until ThemeController (#40).
    
    Closes #54

---
+++ lib/core/theme/querya_theme_scope.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/widgets.dart';
+
+import 'querya_editor_theme.dart';
+import 'querya_theme.dart';
+import 'querya_workbench_theme.dart';
+
+/// Provides [QueryaTheme] (workbench + editor tokens) below [ShadcnApp].
+class QueryaThemeScope extends InheritedWidget {
+  const QueryaThemeScope({
+    super.key,
+    required this.data,
+    required super.child,
+  });
+
+  final QueryaTheme data;
+
+  static QueryaTheme of(BuildContext context) {
+    final scope = context.dependOnInheritedWidgetOfExactType<QueryaThemeScope>();
+    assert(scope != null, 'QueryaThemeScope not found in context');
+    return scope!.data;
+  }
+
+  static QueryaTheme? maybeOf(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<QueryaThemeScope>()
+        ?.data;
+  }
+
+  @override
+  bool updateShouldNotify(QueryaThemeScope oldWidget) =>
+      data != oldWidget.data;
+}
+
+/// Convenient access to [QueryaTheme] tokens from [BuildContext].
+extension QueryaThemeContext on BuildContext {
+  QueryaTheme get queryaTheme => QueryaThemeScope.of(this);
+
+  QueryaWorkbenchTheme get workbench => queryaTheme.workbench;
+
+  QueryaEditorTheme get editorTheme => queryaTheme.editor;
+}
