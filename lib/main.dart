9bd7d8b1bbc356a8c131616cb6840812e053e03d
ZhuchkaTriplesix <mrlololoshka94@gmail.com>
2026-06-18 21:19:46 +0300

    chore: remove unnecessary foundation.dart import to fix lint

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:bitsdojo_window/bitsdojo_window.dart';
-import 'package:flutter/foundation.dart';
+
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
